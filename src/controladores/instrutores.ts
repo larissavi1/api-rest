06d541b588d0f142ed8500388e97ed3882d7c3a4
unknown <you@example.com>
2024-10-30 02:45:40 -0300

    quarta e quinta tarefa ok

--- src/controladores/instrutores.ts
+++ src/controladores/instrutores.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express"
+import rotas from "../rotas"
  
 type Tinstrutores = {
      id: number, nome: string,  email: string }
+
+let proximoIdentificador = 4
 
 const instrutores: Tinstrutores[] = [
      {id: 1, nome: 'guido', email:'guido@email.com'},
@@ -29,10 +32,36 @@ export function cadastrar (req: Request, res:Response) {
     console.log(req.body)
     const {nome, email} = req.body
     const novoinstrutor = {
-        id: 4,
+        id: proximoIdentificador++,
         nome,
         email}
     instrutores.push (novoinstrutor)
-    res.status(201).json(novoinstrutor)
-}
+    res.status(201).json(novoinstrutor)}
 
+// editar um instrutor
+export function editar (req: Request, res:Response) {
+    const { id } = req.params
+    const {nome, email} = req.body
+         const instrutor = instrutores.find((item)=> {
+         return item.id === Number(id)})
+
+             if (!instrutor){
+              res.status(404).json({
+              mensagem: 'instrutor(a) não encontrado(a)'})}
+
+     //instrutor.nome = nome
+     //instrutor.email = email
+         res.status(204).send()}
+
+// excluir um instrutor
+export function excluir (req: Request, res:Response) {
+    const { id } = req.params
+         const instrutorIndice = instrutores.findIndex((item)=> {
+         return item.id === Number(id)})
+
+             if (instrutorIndice === -1){
+              res.status(404).json({
+              mensagem: 'instrutor(a) não encontrado(a)'})}
+
+instrutores.splice(instrutorIndice, 1)
+         res.status(204).send()}
--- src/rotas.ts
+++ src/rotas.ts
@@ -1,9 +1,5 @@
-// editar um instrutor
-// excluir um instrutor
-//cadastrar uma aula a um instrutor
-//excluir a aula para um instrutor
 import { Router } from "express"; 
-import { cadastrar, detalhar,listar,} from "./controladores/instrutores";
+import { cadastrar, detalhar,editar,excluir,listar,} from "./controladores/instrutores";
 
 const rotas = Router()
 
@@ -16,15 +12,16 @@ rotas.get('/instrutores/:id', detalhar)
 // cadastrar um instrutor
 rotas.post ('/cadastrar-instrutor',cadastrar)
 
+// editar um instrutor
+rotas.put('/editar-instrutor/:id', editar ) //ver o erro depois
+
+// excluir um instrutor
+rotas.delete('/deletar-instrutor/:id',excluir)
+
+//cadastrar uma aula a um instrutor
 
 
-
-
-
-
-
-
-
+//excluir a aula para um instrutor
 
 
 
