50123d6667086e55603cf17b2b555847da29f46e
unknown <you@example.com>
2024-10-29 23:43:48 -0300

    segunda tarefa ok

--- src/controladores/instrutores.ts
+++ src/controladores/instrutores.ts
@@ -8,7 +8,20 @@ const instrutores: Tinstrutores[] = [
      {id: 2, nome: 'maria', email:'maria@email.com'},
      {id: 3, nome: 'joao', email:'joao@email.com'}]
 
+// listar todos os instrutores
+export function listar (req: Request, res:Response) {
+      res.status(200).json(instrutores)}
 
-export function listarInstrutores (req: Request, res:Response) {
-     res.status(200).json(instrutores)
+
+// detalhar as info de um instrutor
+ export function detalhar (req: Request, res:Response) {
+    const { id } = req.params
+         const instrutor = instrutores.find((item)=> {
+         return item.id === Number(id)})
+
+             if (!instrutor){
+              res.status(404).json({
+              mensagem: 'instrutor não encontrado(a)'})}
+
+     res.status(200).json(instrutor)
 }
--- src/rotas.ts
+++ src/rotas.ts
@@ -1,17 +1,18 @@
-// detalhar as info de um instrutor
 // cadastrar um instrutor
 // editar um instrutor
 // excluir um instrutor
 //cadastrar uma aula a um instrutor
 //excluir a aula para um instrutor
-
 import { Router } from "express"; 
-import { listarInstrutores } from "./controladores/instrutores";
+import { detalhar,listar,} from "./controladores/instrutores";
+
+const rotas = Router()
 
 // listar todos os instrutores
-const rotas = Router()
-rotas.get  ('/instrutores',listarInstrutores)
+rotas.get  ('/instrutores',listar)
 
+// detalhar as info de um instrutor
+rotas.get('/instrutores/:id', detalhar)
 
 
 
